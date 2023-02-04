3cb877eefcf1bd66ee17b839a9d545b28514a0cb
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 21:00:53 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -6,25 +6,25 @@ let name = await conn.getName(who)
 
 
 if (command == 'owner') {
- let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Saya Owner Maxxy;Bot;;Md\nFN:Saya Owner Maxxy Bot WhatsApp, Md\nNICKNAME:👑 Maxxy Hinata Bot\nORG:Kahfi\nTITLE:soft\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://lynk.id/kahfi.xd\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:maxxyofc@mail.com\nitem3.X-ABLabel:💌 Mail Owner MaxxyBotz\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 13 January 2001\nEND:VCARD`
+ let vcard = `BEGIN:VCARD\VERSION:3.0\nO:WhatsApp;I am Owner Ibrahim;Bot;;Md\nFN:I am the owner of Ibrahim Bot WhatsApp, Md\nNICKNAME:👑 ibrahim bot\nORG:Kahfi\nTITLE:soft\nitem1.TEL;waid=923229931076:+92 322-9931076\nitem1.X-ABLabel:📞 Owner Number\nitem2.URL:https://iamibrahim.tk\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET:theworldh95@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Ibrahim bot\nitem4.ADR:;;pkr Pakistan;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍My Location\nBDAY;value=date:🔖 13 January 2001\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
-let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
+let caption = `👋 Hi *${name} @${who.split("@")[0]}*, Here is the owner *${conn.user.name}* sis`
     await conn.sendButton(m.chat, caption, author, null, [['🎀 Sapa Owner', 'Huuu']], m, { quoted: tag_own, mentions: conn.parseMention(caption) })
 }
 if (command == 'pengembang') {
-  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6285380166282:+62 853-8016-6282\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=923229931076:+92 322-9931076\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Here is my developer kack who activated me.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
 await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fakes })
 }
 if (command == 'creator') {
   try {
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `maxxyofc@gmail.com`, `🇮🇩 Indonesia`, `🚀 https://Frezzy-XD.github.io/`, `👤 Owner Gua Ganteng kok`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `🇮🇩 Indonesia`, `🚀 https://lynk.id/kahfi.xd/`, `🤖 Hanya bot biasa yang kadang suka eror ☺`]
+    [`${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, `👑 Developer Bot `, `🚫 Don't call me 🥺`, `theworldh95@gmail.com`, `pk Pakistan`, `🚀 https://iamibrahim.tk`, `👤 The owner of the cave is handsome`],
+    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🔥 Bot WhatsApp 🐣`, `📵 Don't spam/call me 😢`, `Nothing`, `pk Pakistan`, `🚀 https://iamibrahim.tk`, `🤖 Just ordinary bots that sometimes have errors ☺`]
   ], fakes)
-  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya ka😉`, sentMsg, {mentions: [m.sender]})
+  await conn.reply(m.chat, `Hello sis @${m.sender.split(`@`)[0]} That's my owner's number, don't spam it😉`, sentMsg, {mentions: [m.sender]})
   } catch {
   const sentMsg = await conn.sendContact(m.chat, `${nomorown}`, `${await conn.getName(nomorown+'@s.whatsapp.net')}`, m)
-  await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor team developerku, jangan di apa-apain ya kak😖`, sentMsg, {mentions: [m.sender]})
+  await conn.reply(m.chat, `Hello sis @${m.sender.split(`@`)[0]} That's my developer team number, don't do anything wrong, sis😖`, sentMsg, {mentions: [m.sender]})
   }
   }
 }
