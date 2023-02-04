32d922686b69f42e8e8f5243e31746e97b882a1a
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 13:45:21 +0500

    Update anti-call.js

--- plugins/anti-call.js
+++ plugins/anti-call.js
@@ -19,8 +19,8 @@ export async function all(m, {
 	let chat = global.db.data.chats[m.chat]
 	if(chat.antiCall) return
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		let cap = 'Kamu Di banned + block + warn + kick oleh bot karena telah melanggar aturan bot\n\n*📮Dilarang menelepon Bot!*'
-        await conn.send2ButtonDoc(m.chat, cap, author, '🔖 Matikan Fitur', '.off anticall', 'ℹ️ Menu', '.menu', null, adReply)
+		let cap = 'You are banned + block + warn + kick by bots for violating bot rules\n\n*📮Do not call bots!*'
+        await conn.send2ButtonDoc(m.chat, cap, author, '🔖 Turn off Features', '.off anticall', 'ℹ️ Menu', '.menu', null, adReply)
 		await delay(1000)
 		global.db.data.users[ban].banned = true
         global.db.data.users[ban].warning = 1
