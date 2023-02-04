c22917848ab918a259c93753db0aa5a76bbd119a
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 12:35:30 +0500

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -2,21 +2,21 @@ let handler = async (m, { usedPrefix, text }) => {
     conn.absen = conn.absen ? conn.absen : {}
     let id = m.chat
     if (id in conn.absen) {
-        await conn.sendButton(m.chat, `_*Masih ada absen di chat ini!*_\n\n*${usedPrefix}hapusabsen* - untuk menghapus absen`, author, null, [
-                ['hapusabsen', `${usedPrefix}hapusabsen`]
+        await conn.sendButton(m.chat, `_*There are still absences in this chat!*_\n\n*${usedPrefix}deleteabsent* - to delete absences`, author, null, [
+                ['deleteabsent', `${usedPrefix}deleteabsent`]
             ], m)
     }
     conn.absen[id] = [
-        await conn.sendButton(m.chat, `Berhasil memulai absen!\n\n*${usedPrefix}absen* - untuk absen\n*${usedPrefix}cekabsen* - untuk mengecek absen\n*${usedPrefix}hapusabsen* - untuk menghapus data absen`, author, null, [
-                ['absen', `${usedPrefix}absen`]
+        await conn.sendButton(m.chat, `Successfully started roll call!\n\n*${usedPrefix}absent* - for absent\n*${usedPrefix}checkattendance* - to check attendance\n*${usedPrefix}deleteabsent* - to delete attendance data`, author, null, [
+                ['absent', `${usedPrefix}absent`]
             ], m),
         [],
         text
     ]
 }
-handler.help = ['mulaiabsen [teks]']
-handler.tags = ['absen']
-handler.command = /^(start|mulai)absen$/i
+handler.help = ['startabsent [task]']
+handler.tags = ['absent']
+handler.command = /^startabsent$/i
 handler.group = true
 handler.admin = true
 export default handler
