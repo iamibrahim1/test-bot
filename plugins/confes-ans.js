9998b7d2e266d3a62e17a43b2bbe238aa7b1877d
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 20:03:43 +0500

    Update confes-ans.js

--- plugins/confes-ans.js
+++ plugins/confes-ans.js
@@ -21,9 +21,9 @@ handler.all = async function (m) {
 	console.log({ text: m.text })
 	if (mf && (m.text === 'Balas Pesan' || m.text === '') && m.quoted?.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.")
 	
-	let txt = `Hai kak @${mf.dari.split('@')[0]}, kamu menerima balasan nih.\n\nPesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
+	let txt = `Hi bro @${mf.dari.split('@')[0]}, you received this reply.\n\nThe message you sent earlier:\n${mf.pesan}\n\nReply message:\n${m.text}\n`.trim();
 	await this.reply(mf.dari, txt, null).then(() => {
-		m.reply('Berhasil mengirim balasan.')
+		m.reply('Successfully sent reply.')
 		delay(2000)
 		delete this.confess[mf.id]
 		return !0
