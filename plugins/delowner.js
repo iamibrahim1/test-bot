20209d9dfcdc89ae2789142849682b1e21bbfd7b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 21:02:35 +0500

    Update delowner.js

--- plugins/delowner.js
+++ plugins/delowner.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
     else who = m.chat
-    if (!who) throw `tag orangnya!`
+    if (!who) throw `tag the person!`
     if (!global.owner.includes(who.split`@`[0])) throw 'dia bukan Owner !'
     let index = global.owner.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
     global.owner.splice(index, 1)
-    conn.reply(m.chat, `@${who.split('@')[0]} Sekarang bukan owner!`, m, {
+    conn.reply(m.chat, `@${who.split('@')[0]} Now not the owner!`, m, {
         contextInfo: {
             mentionedJid: [who]
         }
