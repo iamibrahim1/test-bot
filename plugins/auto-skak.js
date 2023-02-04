152990b1b81fa69b0e9639ca9c51971eaf38efb9
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 14:40:11 +0500

    Update auto-skak.js

--- plugins/auto-skak.js
+++ plugins/auto-skak.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 
 export async function all(m) {
 if (m.isBaileys) return
-    let MengSkak = /^(skak|halah|cih|cuih|yaha|erorr|kasian|dek|gajelas|bokep)$/i.test(m.text)
+    let MengSkak = /^(check|halah|chih|cuih|yaha|erorr|casian|dek|clearly|bokep)$/i.test(m.text)
         if (MengSkak && m.isGroup) {
             //Teks Skak
 let f = await fetch('https://api.kanye.rest/')
