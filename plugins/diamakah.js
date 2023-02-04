9b32a49c04b7a3f92ebb30d2ffed0c568d21c6b3
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 21:03:33 +0500

    Update diamakah.js

--- plugins/diamakah.js
+++ plugins/diamakah.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
-*Pertanyaan:* ${command} ${text}
-*Jawaban:* ${pickRandom(['di neraka','di surga','di mars','di tengah laut','di dada :v','di hatimu >///<'])}
+*Question:* ${command} ${text}
+*Answer:* ${pickRandom(['in hell','in heaven','on mars','in the middle of the sea','in your chest :v','in your heart >///<'])}
 `.trim(), m, m.mentionedJid ? {
     contextInfo: {
       mentionedJid: m.mentionedJid
     }
   } : {})
 }
-handler.help = ['dimanakah <pertanyaan>']
-handler.tags = ['kerang']
-handler.command = /^dimanakah$/i
+handler.help = ['whereis <question>']
+handler.tags = ['shell']
+handler.command = /^whereis$/i
 handler.owner = false
 
 export default handler
