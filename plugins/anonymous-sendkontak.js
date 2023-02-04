0455f1fc6341a943e14b67fcce07aca1999e12df
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 13:08:37 +0500

    Update anonymous-sendkontak.js

--- plugins/anonymous-sendkontak.js
+++ plugins/anonymous-sendkontak.js
@@ -4,7 +4,7 @@ let handler = async (m, { command, conn, text }) => {
 	this.anonymous = this.anonymous ? this.anonymous : {}
 	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 	let room = Object.values(this.anonymous).find(room => room.check(who))
-	if (!room) throw 'kamu tidak berada di anonymous chat'
+	if (!room) throw 'you are not in anonymous chat'
 	let other = room.other(who)
   var name
   if (text) name = text
@@ -12,13 +12,13 @@ let handler = async (m, { command, conn, text }) => {
 	var number = who.split('@')[0]
 	let tks = `➔ Nomor: ${m.sender.split`@`[0]}
 ➔ Nama: ${name}`
-    this.reply(m.chat, 'Menggirimkan Kontak...')
-	if (other) this.reply(other, `Partner mengirimkan kontak kepadamu`)
+    this.reply(m.chat, 'Sending Contacts...')
+	if (other) this.reply(other, `Partner sends you contacts`)
 	if (other) this.sendHydrated(other, `${htki} ᴀɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛs ${htka}`, tks, await conn.profilePictureUrl(m.sender, 'image').catch(_ => './thumbnail.jpg'), `wa.me/${m.sender.split`@`[0]}`, 'ᴛᴜʀɴ ᴄʜᴀᴛ ᴘᴀʀᴛɴᴇʀ', null,null, [['ʟᴇᴀᴠᴇ', '.leave'],[null,null],[null,null]], 0,  { mentionedJid: [m.sender]})
 }
-handler.help = ['sendkontak']
+handler.help = ['sendcontact']
 handler.tags = 'anonymous'
-handler.command = /^(sendkontak)$/i
+handler.command = /^(send contact)$/i
 handler.private = true
 
 export default handler
