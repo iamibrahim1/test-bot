5bfb1637e91d8fe67ba34f29ffdf6f9be52a71a4
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 20:02:11 +0500

    Update cmd-set.js

--- plugins/cmd-set.js
+++ plugins/cmd-set.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     global.db.data.sticker = global.db.data.sticker || {}
-    if (!m.quoted) throw `Balas stiker dengan perintah *${usedPrefix + command}*`
+    if (!m.quoted) throw `Reply to stickers with commands *${usedPrefix + command}*`
     if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
-    if (!text) throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} tes`
+    if (!text) throw `Use:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} test`
     let sticker = global.db.data.sticker
     let hash = m.quoted.fileSha256.toString('base64')
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
-    if (sticker[hash] && sticker[hash].locked) throw 'Kamu tidak memiliki izin untuk mengubah perintah stiker ini'
+    if (sticker[hash] && sticker[hash].locked) throw 'You do not have permission to change the order of this sticker'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
@@ -18,13 +18,13 @@ let name = await conn.getName(who)
         at: + new Date,
         locked: false,
     }
-    let str = `*Berhasil Set CMD!*`
+    let str = `*Successfully Set CMD!*`
     conn.reply(m.chat, str, m, { contextInfo: {
           externalAdReply :{
     mediaUrl: sig,
     mediaType: 2,
     description: wm, 
-    title: '👋 Hai, ' + name + ' ' + ucapan,
+    title: '👋 Hi, ' + name + ' ' + ucapan,
     body: botdate,
     thumbnail: await(await fetch(pp)).buffer(),
     sourceUrl: sgc
