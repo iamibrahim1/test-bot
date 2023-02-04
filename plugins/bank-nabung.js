4d675c4e2d88efabbc17a14db887ce23af0a4081
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 14:54:13 +0500

    Update bank-nabung.js

--- plugins/bank-nabung.js
+++ plugins/bank-nabung.js
@@ -1,20 +1,20 @@
 const xpperlimit = 1
 let handler = async (m, { conn, command, args }) => {
 	let user = global.db.data.users[m.sender]
-  let count = command.replace(/^nabung/i, '')
+  let count = command.replace(/^saving/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (user.atm == 0) return m.reply('kamu belum mempuyai atm !')
-  if (user.bank > user.fullatm) return m.reply('Uang dibankmu sudah penuh!')
-  if (count > user.fullatm - user.bank) return m.reply('Uangnya ga muat dibank')
+  if (user.atm == 0) return m.reply('you dont have an atm yet!')
+  if (user.bank > user.fullatm) return m.reply('Your bank is full!')
+  if (count > user.fullatm - user.bank) return m.reply('The money doesn't fit in the bank')
   if (global.db.data.users[m.sender].money >= xpperlimit * count) {
     global.db.data.users[m.sender].money -= xpperlimit * count
     global.db.data.users[m.sender].bank += count
-    conn.reply(m.chat, `Sukses menabung sebesar ${count} Money 💹`, m)
-  } else conn.reply(m.chat, `[❗] Uang anda tidak mencukupi untuk menabung ${count} money 💹`, m)
+    conn.reply(m.chat, `Successful saving of ${count} Money 💹`, m)
+  } else conn.reply(m.chat, `[❗] Your money is not enough to save ${count} money 💹`, m)
 }
-handler.help = ['nabung'].map(v => v + ' <jumlah>')
+handler.help = ['saving'].map(v => v + ' <amount>')
 handler.tags = ['rpg']
-handler.command = /^nabung([0-9]+)|nabung|nabungall$/i
+handler.command = /^saving([0-9]+)|saving|savingall$/i
 
 export default handler
