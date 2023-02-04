9cfd1e3d0eff6bccc77ea9bb26b1ea6b92f7f83b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 12:55:04 +0500

    Update angka.js

--- plugins/angka.js
+++ plugins/angka.js
@@ -2,28 +2,28 @@ let bonus = `${Math.floor(Math.random() * 3000)}`.trim()
 
 let handler = async (m, { conn, command, text, args }) => {
 
-    if (args.length == 0) return conn.reply(m.chat, `Harap masukan pilihan angkamu`, m)
+    if (args.length == 0) return conn.reply(m.chat, `Please enter your number choice`, m)
     if (args[0] == '0' || args[0] == '1' || args[0] == '2' || args[0] == '3' || args[0] == '4' || args[0] == '5' || args[0] == '6' || args[0] == '7' || args[0] == '8' || args[0] == '9') {
     let random = `${pickRandom(['2','9','19','25','36','58','70','92','100','500'])}`
 
   conn.reply(m.chat, `
-*「 TEBAK ANGKA 」*
+*「 GUESS THE NUMBERS 」*
 
-Angka Kamu : ${text}
-Angka Bot : ${pickRandom(['0','1','2','3','4','5','6','7','8','9'])}
+Your numbers : ${text}
+Bot Number : ${pickRandom(['0','1','2','3','4','5','6','7','8','9'])}
 
-Apakah Angkamu Dengan Bot Sama?
+Are Your Numbers With Bots The Same?
 
 +${bonus} XP!
 `.trim(), m)
     global.db.data.users[m.sender].exp += bonus * 1
         } else {
-      conn.reply(m.chat, `Pilih Angka 0 sampai 9!`, m)
+      conn.reply(m.chat, `Choose Numbers 0 to 9!`, m)
     }
 }
-handler.help = ['angka'].map(v => v + ' <0-9>')
+handler.help = ['number'].map(v => v + ' <0-9>')
 handler.tags = ['game']
-handler.command = /^angka/i
+handler.command = /^number/i
 
 handler.tigame = true
 handler.fail = null
