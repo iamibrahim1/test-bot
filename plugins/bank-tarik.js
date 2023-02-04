560726f638a92d21c87e442e51e3decd363229b3
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 14:55:22 +0500

    Update bank-tarik.js

--- plugins/bank-tarik.js
+++ plugins/bank-tarik.js
@@ -1,18 +1,18 @@
 const xpperlimit = 1
 let handler = async (m, { conn, command, args }) => {
 	let user = global.db.data.users[m.sender]
-  let count = command.replace(/^tarik/i, '')
+  let count = command.replace(/^pull/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].bank / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (user.atm == 0) return m.reply('kamu belum mempuyai atm !')
+  if (user.atm == 0) return m.reply('you don't have an atm yet !')
   if (global.db.data.users[m.sender].bank >= xpperlimit * count) {
     global.db.data.users[m.sender].bank -= xpperlimit * count
     global.db.data.users[m.sender].money += count
-    conn.reply(m.chat, `Sukses menarik sebesar ${count} Money 💹`, m)
-  } else conn.reply(m.chat, `[❗] Uang dibank anda tidak mencukupi untuk ditarik sebesar ${count} money 💹`, m)
+    conn.reply(m.chat, `Success attracted by ${count} Money 💹`, m)
+  } else conn.reply(m.chat, `[❗] There is not enough money in your bank to withdraw ${count} money 💹`, m)
 }
-handler.help = ['tarik'].map(v => v + ' <jumlah>')
+handler.help = ['pull'].map(v => v + ' <amount>')
 handler.tags = ['rpg']
-handler.command = /^tarik([0-9]+)|tarik|tarikall$/i
+handler.command = /^pull([0-9]+)|pull|pull$/i
 
 export default handler
