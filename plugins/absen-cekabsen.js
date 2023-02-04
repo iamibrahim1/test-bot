480439057f42c7f93b0bf869ca0e9633d85feea5
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 12:31:14 +0500

    Update absen-cekabsen.js

--- plugins/absen-cekabsen.js
+++ plugins/absen-cekabsen.js
@@ -2,8 +2,8 @@
 let handler = async (m, { conn, groupMetadata, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) await conn.sendButton(m.chat, `_*Tidak ada absen berlangsung digrup ini!*_\n\n*${usedPrefix}mulaiabsen* - untuk memulai absen`, author, null, [
-                ['Mulaiabsen', `${usedPrefix}mulaiabsen`]
+    if (!(id in conn.absen)) await conn.sendButton(m.chat, `_*There are no absences in this group!*_\n\n*${usedPrefix}startabsent* - to start absent`, author, null, [
+                ['Startabsent', `${usedPrefix}startabsent`]
             ], m)
             
     let d = new Date
@@ -14,10 +14,10 @@ let handler = async (m, { conn, groupMetadata, usedPrefix }) => {
     })
     let absen = conn.absen[id][1]
     let list = absen.map((v, i) => `${dmenub} ${i + 1}.  @${v.split`@`[0]}`).join('\n')
-            let caption = `*${htjava} TANGGAL ${htjava}*\n${date}
+            let caption = `*${htjava} DATE ${htjava}*\n${date}
 ${conn.absen[id][2]}
 
-*${htjava} SUDAH ABSEN ${htjava}*
+*${htjava} ALREADY ABSENT ${htjava}*
 *Total:* ${absen.length}
 
 ${dmenut}
@@ -25,11 +25,11 @@ ${list}
 ${dmenuf}
 `
 `
-await conn.sendButton(m.chat, caption, author, null, [['absen', `${usedPrefix}absen`], ['cekabsen', `${usedPrefix}cekabsen`]], m, { mentions: conn.parseMention(caption) })
+await conn.sendButton(m.chat, caption, author, null, [['absent', `${usedPrefix}absent`], ['checkattendance', `${usedPrefix}checkattendance`]], m, { mentions: conn.parseMention(caption) })
 
 }
-handler.help = ['cekabsen']
-handler.tags = ['absen']
-handler.command = /^cekabsen$/i
+handler.help = ['checkattendance']
+handler.tags = ['absent']
+handler.command = /^checkattendance$/i
 handler.group = true
 export default handler
