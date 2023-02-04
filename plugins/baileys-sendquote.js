cc6b3096c1d461a2930698468cd11b9a1b42a982
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 14:49:05 +0500

    Update baileys-sendquote.js

--- plugins/baileys-sendquote.js
+++ plugins/baileys-sendquote.js
@@ -1,10 +1,10 @@
 async function handler(m) {
     if (!m.quoted) throw 'reply pesan!'
     let q = await m.getQuotedObj()
-    if (!q.quoted) throw 'pesan yang anda reply tidak mengandung reply!'
+    if (!q.quoted) throw 'The message you replied to does not contain a reply!'
     await q.quoted.copyNForward(m.chat, true)
 }
-handler.help = ['q (reply from teks reply)']
+handler.help = ['q (reply from reply text)']
 handler.tags = ['baileys']
 handler.command = /^q$/i
 
