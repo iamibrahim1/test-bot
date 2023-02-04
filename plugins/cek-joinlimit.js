3bc74032d50aa3a8fade7d47532b4f741e0ca69e
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-04 18:51:41 +0500

    Update cek-joinlimit.js

--- plugins/cek-joinlimit.js
+++ plugins/cek-joinlimit.js
@@ -2,7 +2,7 @@ let handler = async (m) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
     else who = m.sender
-    if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
+    if (typeof db.data.users[who] == 'undefined') throw 'The user doesnt exist in the database'
     m.reply(`${global.db.data.users[who].joinlimit} Join Limit Left=͟͟͞͞🏀`)
 }
 handler.help = ['joinlimit [@user]']
