4f5f61572f3aeb667286230eccb90c16143ea6e7
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:22:40 +0500

    Update fun-kerangajaib.js

--- plugins/fun-kerangajaib.js
+++ plugins/fun-kerangajaib.js
@@ -1,18 +1,18 @@
 let handler = async (m, { text, command, usedPrefix }) => {
     if (!text) throw `Use example ${usedPrefix}${command} i'm alien?`
     m.reply(`"${[
-        'Mungkin suatu hari',
-        'Tidak juga',
-        'Tidak keduanya',
-        'Kurasa tidak',
-        'Ya',
-        'Coba tanya lagi',
-        'Tidak ada'
-    ].getRandom()}."`)
+    'Maybe someday',
+         'Not really',
+         'Not both',
+         'I do not think so',
+         'Yes',
+         'Try asking again',
+         'There isnt any'
+     ].getRandom()}."`)
 }
-handler.help = ['kerang', 'kerangajaib'].map(v => v + ' <teks>')
-handler.tags = ['kerang', 'fun']
+handler.help = ['magicshells'].map(v => v + ' <text>')
+handler.tags = ['shells', 'fun']
 
-handler.command = /^(kulit)?kerang(ajaib)?$/i
+handler.command = /^(magicshells)?$/i
 
 export default handler
