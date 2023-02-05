136ff8a4ab76c823d36945b88203d30c055de72f
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 21:03:09 +0500

    Update group-setWelcome.js

--- plugins/group-setWelcome.js
+++ plugins/group-setWelcome.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.welcome = text
     else if (isOwner) conn.welcome = text
     else global.db.data.chats.sWelcome = text
-    m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)')
-  } else throw 'Teksnya mana?'
+    m.reply('Welcome successfully set up\n@user (Mention)\n@subject (Group Title)')
+   } else throw 'Wheres the text?'
 }
-handler.help = ['setwelcome <teks>']
+handler.help = ['setwelcome <text>']
 handler.tags = ['group']
 handler.command = /^setwelcome$/i
 
