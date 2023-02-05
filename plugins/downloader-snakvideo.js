74b3a73e0c1d6918b72fab3c731e76b667cda54b
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 14:14:29 +0500

    Update downloader-snakvideo.js

--- plugins/downloader-snakvideo.js
+++ plugins/downloader-snakvideo.js
@@ -1,14 +1,17 @@
-
 import fetch from 'node-fetch'
-
-   let handler = async (m, { conn, args }) => {
-	  if (!args[0]) throw 'Input URL'   
-    let res = await fetch(`https://api.lolhuman.xyz/api/snackvideo?apikey=${global.lolkey}&url=${args[0]}`)
-    let json = await res.json()
-    if (json.status != '200') throw Error(anu.message)
-    let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    conn.send2ButtonVid(m.chat, json.result.link, txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
-    }
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `*Usage : ${usedPrefix + command} snak_url_video*\n\nExample :\n${usedPrefix + command} url`
+	if (!(text.includes('http://') || text.includes('https://'))) throw `url invalid, please input a valid url. Try with add http:// or https://`
+	try {
+		let anu = await fetch(`https://api.lolhuman.xyz/api/snackvideo?apikey=${global.lolkey}&url=${text}`)
+		let json = await anu.json()
+		if (anu.status != '200') throw Error(anu.message)
+		await conn.sendMessage(m.chat, { video: { url: json.result.link}, caption: json.result.title }, { quoted: m })
+	} catch (e) {
+		console.log(e)
+		m.reply(`Invalid likee url.`)
+	}
+}
 handler.help = ['Snakvideo']
 handler.tags = ['downloader']
 handler.command = /^snakvideo$/i
