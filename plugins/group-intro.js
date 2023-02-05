64bd49e7ee0d9b53984755dc9797ed693c8cad24
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:51:31 +0500

    Update group-intro.js

--- plugins/group-intro.js
+++ plugins/group-intro.js
@@ -1,6 +1,6 @@
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-let krtu = htki + 'Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ' + htka + '\n' + dmenub + ' *Nama:*\n' + dmenub + ' *Umur:*\n' + dmenub + ' *Alamat:*\n' + dmenub + ' *Hobi:*\n' + dmenub + ' *Pasangan:*\n' + dmenuf
+let krtu = htki + 'Yᴏᴜʀ Cᴀʀᴅ Iɴᴛʀᴏ' + htka + '\n' + dmenub + ' *Name:*Ibrahim\n' + dmenub + ' *age:*27\n' + dmenub + ' *Address:*Pakistan\n' + dmenub + ' *Hobby:*Pthon/javascript\n' + dmenub + ' *Partner:*None\n' + dmenuf
 return conn.sendButtonDoc(m.chat, krtu, wm, 'MENU', '.menu', m, fakeig) // Tambah sendiri kalo mau
 }
 handler.help = ['intro']
