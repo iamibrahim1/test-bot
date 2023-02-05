d2ee26f061a98ead66a03576b66551cb5084bf94
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:04:54 +0500

    Update fun-apakah.js

--- plugins/fun-apakah.js
+++ plugins/fun-apakah.js
@@ -2,14 +2,14 @@ import fetch from 'node-fetch'
 let handler = async (m, { text, args, command }) => {
   if (!args[0]) throw `Use example .${command} halo`
   m.reply(`
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${['Ya', 'Mungkin iya', 'Mungkin', 'Mungkin tidak', 'Tidak', 'Tidak mungkin'].getRandom()}
+*Question:* ${m.text}
+*Answers:* ${['Yes', 'Maybe so', 'Maybe', 'Probably not', 'No', 'Impossible'].getRandom()}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
 }
-handler.help = ['apakah'].map(v => v + ' <teks>')
-handler.tags = ['kerang', 'fun']
-handler.command = /^apakah$/i
+handler.help = ['is'].map(v => v + ' <teks>')
+handler.tags = ['shell', 'fun']
+handler.command = /^is$/i
 
 export default handler
