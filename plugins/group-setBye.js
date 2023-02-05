361e2c7c2a10953189c7bc7a8d800f0206d943aa
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 21:01:26 +0500

    Update group-setBye.js

--- plugins/group-setBye.js
+++ plugins/group-setBye.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
     if (isROwner) global.conn.bye = text
     else if (isOwner) conn.bye = text
     else global.db.data.chats.sBye = text
-    m.reply('Bye berhasil diatur\n@user (Mention)')
-  } else throw 'Teksnya mana?'
+    m.reply('Bye successfully set up\n@user (Mention)')
+   } else throw 'Wheres the text?'
 }
-handler.help = ['setbye <teks>']
+handler.help = ['setbye <text>']
 handler.tags = ['group']
 handler.command = /^setbye$/i
 handler.group = true
