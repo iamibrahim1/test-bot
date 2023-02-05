65073296bdf35ab3553cfa2570ce377a47526253
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 15:00:52 +0500

    Update fitur-bkphub.js

--- plugins/fitur-bkphub.js
+++ plugins/fitur-bkphub.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
-  if (command == 'caribokep') {
-  if (!text) throw `Contoh penggunaan ${usedPrefix}${command} japan`
+  if (command == 'xnxxsearch') {
+  if (!text) throw `Example of use ${usedPrefix}${command} japan`
   let links = `https://api.lolhuman.xyz/api/xnxxsearch?apikey=${global.lolkey}&query=${text}`
 let f = await fetch(links)
 let xx = await f.json()
@@ -15,8 +15,8 @@ Link: ${v.link}`.trim()
             ], fakes, adReply)
     }
     
-    if (command == 'dlbokep') {
-    if (!args[0]) throw `Contoh penggunaan ${usedPrefix}${command} https://www.xnxx.com/video-uy5a73b/mom_is_horny_-_brooklyn`
+    if (command == 'xnxxdl') {
+    if (!args[0]) throw `Example of use ${usedPrefix}${command} https://www.xnxx.com/video-uy5a73b/mom_is_horny_-_brooklyn`
     try {
   let json = await fetch(`https://api.lolhuman.xyz/api/xnxx?apikey=${global.lolkey}&url=${text}`)
   let x = await json.json()
@@ -38,8 +38,8 @@ throw eror
 
 }
 
-handler.help = ['caribokep', 'dlbokep'].map(v => v + ' <app>')
-handler.command = ['caribokep', 'dlbokep']
+handler.help = ['xnxxdl', 'xnxxsearch'].map(v => v + ' <app>')
+handler.command = ['xnxxsearch', 'xnxxdl']
 handler.tags = ['nsfw']
 
 export default handler
