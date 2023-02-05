1700d396f7b2ccc8ae9911263de5266a67a02250
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:53:36 +0500

    Update group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -2,13 +2,13 @@ import { areJidsSameUser } from '@adiwajshing/baileys'
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
-    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Hanya bisa dibuka di grup'
+    if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw 'Can only be opened in groups'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants is not defined :('
     let me = groupMetadata.participants.find(user => areJidsSameUser(user.id, conn.user.id))
-    if (!me) throw 'Gw gk ada di grup itu 🗿'
-    if (!me.admin) throw 'Gw bukan atmin bg 🗿'
+    if (!me) throw 'Im not in that group 🗿'
+    if (!me.admin) throw 'Im not admin bro 🗿'
     await conn.sendHydrated(m.chat, `*${htki} L I N K ${htka}*`, botdate, null, 'https://www.whatsapp.com/otp/copy/'+'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), 'C O P Y', nomorown, nameown, [
       ['LIST GROUP', '/listgroup'],
       ['ANTILINK', '/on antilink']
