bfa0eae5f1333917f1c78bac98a7753724c2900a
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 14:51:45 +0500

    Update exp-unreg.js

--- plugins/exp-unreg.js
+++ plugins/exp-unreg.js
@@ -1,6 +1,6 @@
 import { createHash } from 'crypto'
 let handler = async function (m, { args }) {
-  if (!args[0]) throw 'Serial Number kosong'
+  if (!args[0]) throw 'Serial number is blank'
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
   if (args[0] !== sn) throw 'Serial Number salah'
