eddd9b94ca154c57f267168f39dc778211f9db20
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:01:55 +0500

    Update fitur-yang.js

--- plugins/fitur-yang.js
+++ plugins/fitur-yang.js
@@ -1,18 +1,18 @@
 let handler = async (m, { conn, command, usedPrefix, text, groupMetadata }) => {
 
-if (!text) throw `Contoh:
-${usedPrefix + command} Yang terserah`
+if (!text) throw `Example:
+${usedPrefix + command} Which is up`
 let em = ['😀','😂','😃','🗿','🤤','😁','😐','🙂','☹️']
 
     let toM = a => '@' + a.split('@')[0]
     let ps = groupMetadata.participants.map(v => v.id)
     let a = ps.getRandom()
     let am = em.getRandom()
-    conn.sendButton(m.chat, `Yang Paling *${text}* Adalah ${toM(a)} ${am}`, author, null, [['Cari lagi', '.yang ' + text]], m,{mentions: [a]})
+    conn.sendButton(m.chat, `The most *${text}* Is ${toM(a)} ${am}`, author, null, [['search again', '.yang ' + text]], m,{mentions: [a]})
     
 }
-handler.help = ['yang'].map(v => v + ' <teks>')
-handler.command = ['yang']
+handler.help = ['Which '].map(v => v + ' <text>')
+handler.command = ['Which']
 handler.tags = ['fun']
 
 handler.group = true
