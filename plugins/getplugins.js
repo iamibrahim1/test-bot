bc204c054dacdd7813ea63fe569ef6b20d38dc95
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:44:58 +0500

    Update getplugins.js

--- plugins/getplugins.js
+++ plugins/getplugins.js
@@ -2,8 +2,8 @@ import fs from 'fs'
 let handler = async (m, { usedPrefix, command, text }) => {
     let ar = Object.keys(plugins)
     let ar1 = ar.map(v => v.replace('.js', ''))
-    if (!text) throw `uhm.. teksnya mana?\n\ncontoh:\n${usedPrefix + command} creator`
-    if (!ar1.includes(text)) return m.reply(`'${text}' tidak ditemukan!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
+    if (!text) throw `uhm.. where is the text?\n\nexample:\n${usedPrefix + command} creator`
+    if (!ar1.includes(text)) return m.reply(`'${text}' not found!\n\n${ar1.map(v => ' ' + v).join`\n`}`)
     m.reply(fs.readFileSync('./plugins/' + text + '.js', 'utf-8'))
 }
 handler.help = ['getplugin'].map(v => v + ' <teks>')
