48f8702a349917295201c8b11f03b59d6f6a2112
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:40:55 +0500

    Update game-asahotak-ans.js

--- plugins/game-asahotak-ans.js
+++ plugins/game-asahotak-ans.js
@@ -6,27 +6,27 @@ export async function before(m) {
         return !0
     this.asahotak = this.asahotak ? this.asahotak : {}
     if (!(id in this.asahotak))
-        return conn.sendButton(m.chat, 'Soal itu telah berakhir', author, null, buttonasahotak, m)
+        return conn.sendButton(m.chat, 'The matter is over', author, null, buttonasahotak, m)
     if (m.quoted.id == this.asahotak[id][0].id) {
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (isSurrender) {
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
-            return conn.sendButton(m.chat, '*Yah Menyerah :( !*', author, null, buttonasahotak, m)
+            return conn.sendButton(m.chat, '*Well Surrender :( !*', author, null, buttonasahotak, m)
         }
         let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
         // m.reply(JSON.stringify(json, null, '\t'))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
-            conn.sendButton(m.chat, `*Benar!*\n+${this.asahotak[id][2]} XP`, author, null, buttonasahotak, m)
+            conn.sendButton(m.chat, `*Correct!*\n+${this.asahotak[id][2]} XP`, author, null, buttonasahotak, m)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold)
             m.reply(`*Dikit Lagi!*`)
         else
             conn.sendButton(m.chat, `*Salah!*`, author, null, [
-                ['Hint', '/hasa'],
-                ['Nyerah', 'menyerah']
+             ['Hint', '/hasa'],
+             ['give up', 'give up']
             ], m)
     }
     return !0
@@ -34,5 +34,5 @@ export async function before(m) {
 export const exp = 0
 
 const buttonasahotak = [
-    ['asahotak', '/asahotak']
+    ['brainteaser', '/brainteaser']
 ]
