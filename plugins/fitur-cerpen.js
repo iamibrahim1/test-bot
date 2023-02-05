edc081465c190070b87a3ca62958f141e9249bc0
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 17:34:02 +0500

    Update fitur-cerpen.js

--- plugins/fitur-cerpen.js
+++ plugins/fitur-cerpen.js
@@ -6,16 +6,16 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
         let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
         let name = await conn.getName(who)
         let cerpen = await Cerpen()
-        let hasil = `${htki} Cerpen *${text}* ${htka}
+        let hasil = `${htki} Shortstory *${text}* ${htka}
 
 ${cerpen}
 
 *${htjava} By:* ${author}
 `.trim()
-return conn.sendButton(m.chat, hasil, wm, logo, [['➡️Next Cerpen➡️', `${usedPrefix}${command} ${text}`]], m, fakefb)
+return conn.sendButton(m.chat, hasil, wm, logo, [['➡️Next shortstory➡️', `${usedPrefix}${command} ${text}`]], m, fakefb)
 }
-handler.help = ['cerpen'].map(v => v + ' <apa>')
-handler.tags = ['edukasi']
-handler.command = /^(cerpen|cerita)$/i
+handler.help = ['Shortstory'].map(v => v + ' <apa>')
+handler.tags = ['education']
+handler.command = /^shortstory$/i
 
 export default handler
