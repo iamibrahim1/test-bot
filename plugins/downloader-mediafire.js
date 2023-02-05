35671c8dc77a75fcb63a9ad58498a5c7b40102c8
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 12:37:37 +0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -2,7 +2,7 @@ import { mediafiredl } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
+if (!args[0]) throw `Use example ${usedPrefix}${command} any mediafire link`
 try {
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
