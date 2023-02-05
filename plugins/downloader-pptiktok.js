32e9e78e1da6b30a68f6375632d76aeb02ee1d7e
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 12:40:59 +0500

    Update downloader-pptiktok.js

--- plugins/downloader-pptiktok.js
+++ plugins/downloader-pptiktok.js
@@ -1,8 +1,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, text }) => {
-if (!text) throw '*[❗INFO❗] Masukan Nama User Tiktok Yang Ingin Diambil Fotonya*'
+if (!text) throw '*[❗INFO❗] Enter the Tiktok username you want to take a photo of*'
 let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${global.lolkey}`
-conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ] Sukses Mengambil Foto User ${text}*`, m, false)
+conn.sendFile(m.chat, res, 'error.jpg', `*[ ✔ ] Success Taking User Photos ${text}*`, m, false)
 }
 handler.help = ['tiktokfoto'].map(v => v + ' <user>')
 handler.tags = ['downloader']
