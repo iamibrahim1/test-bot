8f286b12d7d9306ee119b03b290bf26a07cb8978
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 19:56:02 +0500

    Update fitur-spamwa.js

--- plugins/fitur-spamwa.js
+++ plugins/fitur-spamwa.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text }) => {
 
-let [nomor, pesan, jumlah] = text.split('|')
-if (!nomor) throw '*[ ⚠️ ] HARAP MASUKKAN NOMOR YANG AKAN DI SPAM PESAN!*\n*PENGGUNAAN YANG BENAR:*\n*—◉ #spamwa nomor|teks|jumlah*\n*jumlah:*\n*—◉ #spamwa 628xxxxx|hai :v|25*'
-if (!pesan) throw '*[ ⚠️ ] HARAP MASUKKAN PESAN KE SPAM!*\n*GUNAKAN YANG BENAR:*\n*—◉ #spamwa nomor|teks|jumlah*\n*CONTOH:*\n*—◉ #spamwa 628xxxxx|merespons :v|25*'
-if (jumlah && isNaN(jumlah)) throw '*[ ⚠️ ] KUANTITAS HARUS ADALAH NOMOR!*\n*PENGGUNAAN YANG TEPAT:*\n*—◉ #spamwa nomor|teks|jumlah*\n*CONTOH:*\n*—◉ #spamwa 628xxxxx|merespons :v|25*'
+let [number, message, amount] = text.split('|')
+if (!number) throw '*[ ⚠️ ] PLEASE ENTER THE NUMBER TO SPAM THE MESSAGE!*\n*CORRECT USE:*\n*—◉ #spamwa number|text|amount*\n*amount:*\n *—◉ #spamwa 923xxxxxxxxx|hi :v|25*'
+if (!message) throw '*[ ⚠️ ] PLEASE PUT THE MESSAGE INTO SPAM!*\n*USE CORRECT:*\n*—◉ #spamwa number|text|amount*\n*EXAMPLE:*\n*—◉ #spamwa 923xxxxxxxxx|respond :v|25*'
+if (!amount&& isNaN(amount)) throw '*[ ⚠️ ] QUANTITY MUST BE A NUMBER!*\n*PROPER USE:*\n*—◉ #spamwa number|text|amount*\n*EXAMPLE:*\n *—◉ #spamwa 923xxxxxxxxx|respond :v|25*'
 
-let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
-let fixedJumlah = jumlah ? jumlah * 1 : 10
-if (fixedJumlah > 10) throw '*[ ⚠️ ] TERLALU BANYAK PESAN! JUMLAH HARUS KURANG DARI 10 PESAN*️'
-await m.reply(`*[❗] SPAM PESAN KE NOMOR ${nomor} ITU SUKSES DILAKUKAN*\n*JUMLAH TERKIRIM:*\n*—◉ ${fixedJumlah} waktu!*`)
+let fixedNumber = number.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
+let fixedJumlah = amount ? amount * 1 : 10
+if (fixedJumlah > 10) throw '*[ ⚠️ ] TOO MANY MESSAGES! QUANTITY MUST BE LESS THAN 10 MESSAGES*️'
+await m.reply(`*[❗] SPAM MESSAGE TO NUMBER ${number} WAS SUCCESSFULLY DONE*\n*NUMBER SENT:*\n*—◉ ${fixedJumlah} waktu!*`)
 for (let i = fixedJumlah; i > 1; i--) {
-if (i !== 0) conn.reply(fixedNumber, pesan.trim(), m)
+if (i !== 0) conn.reply(fixedNumber, message.trim(), m)
 }}
 handler.help = ['spamwa <number>|<mesage>|<no of messages>']
 handler.tags = ['tools']
