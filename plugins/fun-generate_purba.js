abb53b1e4ff50f4678ccf78e39ff3bfdb279f4dd
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-05 20:13:16 +0500

    Update fun-generate_purba.js

--- plugins/fun-generate_purba.js
+++ plugins/fun-generate_purba.js
@@ -2,9 +2,9 @@ function handler(m, { text }) {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
     m.reply(teks.replace(/[aiueo]/gi, '$&ve'))
 }
-handler.help = ['purba'].map(v => v + ' <teks>')
+handler.help = ['ancient'].map(v => v + ' <teks>')
 handler.tags = ['fun']
-handler.command =  /^(purba)$/i
+handler.command =  /^(ancient)$/i
 
 
 export default handler
