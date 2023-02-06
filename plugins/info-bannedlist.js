83c11ccd0555c6b173f0cacafa54f5d95646d8d6
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 11:00:17 +0500

    Update info-bannedlist.js

--- plugins/info-bannedlist.js
+++ plugins/info-bannedlist.js
@@ -3,14 +3,14 @@ let handler = async (m, { jid, conn, usedPrefix }) => {
     let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
     
-    conn.reply(m.chat, `${cmenut} *Daftar Chat Terbanned*
+    conn.reply(m.chat, `${cmenut} *Banned chat list*
 │ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
 `.trim()).join('\n') : ''}
 ${cmenuf}
 
-${cmenut} *Daftar User Terbanned*
+${cmenut} *List of Banned Users*
 │ Total : ${users.length} User${users ? '\n' + users.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
