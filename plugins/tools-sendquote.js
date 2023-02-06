3fa64670f9dd3dd583d2c3451a2189ff7e9f8db1
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 14:19:30 +0500

    Update tools-sendquote.js

--- plugins/tools-sendquote.js
+++ plugins/tools-sendquote.js
@@ -5,10 +5,10 @@ async function handler(m, { isAdmin, isOwner }) {
             throw false
         }
     }
-    if (!m.quoted) throw 'balas pesannya!'
-    let q = this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw 'pesan yang kamu balas tidak mengandung balasan!'
-    await q.quoted.copyNForward(m.chat, true)
+  if (!m.quoted) throw 'reply to the message!'
+     let q = this.serializeM(await m.getQuotedObj())
+     if (!q.quoted) throw 'the message you replied to did not contain a reply!'
+     await q.quoted.copyNForward(m.chat, true)
 }
 handler.help = ['q']
 handler.tags = ['tools']
