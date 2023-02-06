8fa8d3af8c009aa7c69284233638d909ee2b2bc4
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 13:57:29 +0500

    Update vote-vote.js

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -2,13 +2,13 @@
 let handler = async (m, { conn, groupMetadata, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) await conn.sendButton(m.chat, `Tidak ada voting digrup ini!`, author, null, [
+    if (!(id in conn.vote)) await conn.sendButton(m.chat, `There is no voting in this group!`, author, null, [
     ['vote', `${usedPrefix}+vote`],
     ['venu', `${usedPrefix}menu`]
 ], m)
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
-    if (wasVote) throw 'Kamu sudah vote!'
+    if (wasVote) throw 'You have voted!'
     if (/up/i.test(command)) {
         conn.vote[id][1].push(m.sender)
     } else if (/de/i.test(command)) {
