7066b59c575f78737ddd9f9b481e03f750975893
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 18:12:55 +0500

    Update tools-listnote.js

--- plugins/tools-listnote.js
+++ plugins/tools-listnote.js
@@ -1,19 +1,19 @@
 let handler = async(m, {conn, command, usedPrefix, text}) => {
   global.db.data.users[m.sender].catatan = global.db.data.users[m.sender].catatan || []
   let i = 0
-  if (global.db.data.users[m.sender].catatan.length == 0) return m.reply('Kamu belum punya catatan!')
+  if (global.db.data.users[m.sender].catatan.length == 0) return m.reply('You dont have any notes yet!')
   let txt = '🗒️Daftar catatan\n\n'
   for (let ct in global.db.data.users[m.sender].catatan) {
     i += 1
     txt += '*' + i + '.* ' + global.db.data.users[m.sender].catatan[ct].title + '\n'
   }
-  if (text.length == 0) return await conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/15e31900512863624ed57.jpg', txt, 'Penggunaan:\n' + usedPrefix + 'lihatcatatan 1\nHapus catatan:\n' + usedPrefix + 'hapuscatatan 1', 'Okey', 'Ok', m)
+  if (text.length == 0) return await conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/15e31900512863624ed57.jpg', txt, 'Use:\n' + usedPrefix + 'seenote 1\Delete record:\n' + usedPrefix + 'hapuscatatan 1', 'Okey', 'Ok', m)
   let catatan = global.db.data.users[m.sender].catatan
   let split = text.split('|')
-  if (catatan.length == 0) return m.reply('Kamu belum memiliki catatan!')
+  if (catatan.length == 0) return m.reply('You dont have any notes yet!')
   let n = Number(split[0]) - 1
 
-  let isi = global.db.data.users[m.sender].catatan[n] != undefined ? global.db.data.users[m.sender].catatan[n].isi : 'Catatan tidak ditemukan!'
+  let isi = global.db.data.users[m.sender].catatan[n] != undefined ? global.db.data.users[m.sender].catatan[n].isi : 'Records not found!'
 conn.reply(m.chat, `${isi}`, m, false, {
     contextInfo: {
       mentionedJid: conn.parseMention(text)
@@ -21,8 +21,8 @@ conn.reply(m.chat, `${isi}`, m, false, {
   })
 }
 
-handler.help = ['lihatcatatan']
+handler.help = ['seenote']
 handler.tags = ['tools']
-handler.command = /^(li(hat(c(atatan|tt)|note)|atnote)|bukac(atatan|tt))$/i
+handler.command = /^seenote$/i
 
 export default handler
