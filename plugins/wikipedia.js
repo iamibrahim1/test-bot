8428b4c6e1a8e7f8e89113660ea050564ba70470
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 13:49:39 +0500

    Update wikipedia.js

--- plugins/wikipedia.js
+++ plugins/wikipedia.js
@@ -1,6 +1,6 @@
 import { wikipedia } from '@bochilteam/scraper'
 let handler = async (m, { text, usedPrefix, command }) => {
-  if (!text) throw `Contoh penggunaan ${usedPrefix}${command} Minecraft`
+  if (!text) throw `Usage examples ${usedPrefix}${command} Minecraft`
   let json = await wikipedia(text)
   m.reply(`
 *${json.title}*
@@ -9,7 +9,7 @@ ${json.img}
 ${json.articles}
 `.trim())
 }
-handler.help = ['wikipedia'].map(v => v + ' <apa>')
+handler.help = ['wikipedia'].map(v => v + ' <text>')
 handler.tags = ['internet']
 handler.command = /^(wiki|wikipedia)$/i
 
