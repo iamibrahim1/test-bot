04f5439f7d3826d1850e85b83b12e9a71e1f44ff
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 12:48:26 +0500

    Update islami-ayatmp32.js

--- plugins/islami-ayatmp32.js
+++ plugins/islami-ayatmp32.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { args, usedPrefix, command }) => {
-    if (!(args[0] || args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 beserta audionya, dan ayatnya 1 aja`
-    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nmaka hasilnya adalah surah Al-Fatihah ayat 2 beserta audionya, dan ayatnya 1 aja`
+    if (!(args[0] || args[1])) throw `example:\no${usedPrefix + command} 1 2\x\then the result is sura Al-Fatihah verse 2 along with the audio, and the verse is just 1`
+    if (isNaN(args[0]) || isNaN(args[1])) throw `example:\no${usedPrefix + command} 1 2\x\then the result is sura Al-Fatihah verse 2 along with audio, and only verse 1`
     
 let api = `https://api.lolhuman.xyz/api/quran/audio/args[0]/args[1]?apikey=${global.lolkey}`
 await conn.sendFile(m.chat, api, 'quran.mp3', null, m, true, {
@@ -11,7 +11,7 @@ ptt: true
 
 }
 
-handler.help = ['ayta2'].map(v => v + ' *surah no*')
+handler.help = ['ayat2'].map(v => v + ' *surah no*')
 handler.tags = ['islam']
 handler.command = /^(ayat(mp32|audio2)|ayta2)$/i
 export default handler
