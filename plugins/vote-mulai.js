35e1d8378dc57e875a10ac9a108eb9d4a937efad
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 13:58:09 +0500

    Update vote-mulai.js

--- plugins/vote-mulai.js
+++ plugins/vote-mulai.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, text, usedPrefix, isAdmin, isOwner }) => {
     }
     conn.vote = conn.vote ? conn.vote : {}
     let id = m.chat
-    if (id in conn.vote) await conn.sendButton(m.chat, `*Masih ada vote di chat ini!*`, author, null, [
+    if (id in conn.vote) await conn.sendButton(m.chat, `*There are still votes in this chat!*`, author, null, [
                 ['hapus', `${usedPrefix}-vote`]
             ], m)
             let caption = `${htjava} MULAI VOTE ${htjava}
-${dmenub} *${usedPrefix}upvote* - untuk setuju
-${dmenub} *${usedPrefix}devote* - untuk tidak setuju
-${dmenub} *${usedPrefix}cekvote* - untuk mengecek vote
-${dmenub} *${usedPrefix}hapusvote* - untuk menghapus vote
+${dmenub} *${usedPrefix}upvote* - to agree
+${dmenub} *${usedPrefix}devote* - to disagree
+${dmenub} *${usedPrefix}checkvote* - to check vote
+${dmenub} *${usedPrefix}removevote* - to remove the vote
 ${dmenuf}`
             await conn.sendButton(m.chat, caption, author, null, [
                 ['upvote', `${usedPrefix}upvote`],
@@ -23,7 +23,7 @@ ${dmenuf}`
         []
     ]
 }
-handler.help = ['mulaivote [alasan]']
+handler.help = ['startvote [reason]']
 handler.tags = ['vote']
 handler.command = /^(start|mulai|\+)vote$/i
 
