a355de23050a5189d7a41a6151926192404f436d
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-06 18:07:56 +0500

    Update tools-qrcode.js

--- plugins/tools-qrcode.js
+++ plugins/tools-qrcode.js
@@ -5,12 +5,12 @@ let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 
-let caption = `Nih hasil ${command} nya
-Balas pesan ini ntuk membaca Qr Ketik *.readqr* `
+let caption = `Here is the result of the ${command}
+Reply to this message to read Qr Type *.readqr* `
 await conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', caption, m)
 
 }
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
 
