087f9da0069e268b7bdc755ca744323eb41deac8
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-09 12:31:00 +0500

    Update tools-buatnote.js

--- plugins/tools-buatnote.js
+++ plugins/tools-buatnote.js
@@ -1,23 +1,23 @@
 let handler = async(m, { conn, command, usedPrefix, text }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
-  let fail = 'Format salah, example:\n' + usedPrefix + command + ' ' + name + '|1. Masak\n2. Makan'
+  let fail = 'Incorrect format, example:\n' + usedPrefix + command + ' ' + name + '|1. Cook\n2. Eat'
   global.db.data.users[m.sender].catatan = global.db.data.users[m.sender].catatan || []
   let catatan = global.db.data.users[m.sender].catatan
   let split = text.split('|')
   let title = split[0]
   let isi = split[1]
-  if (catatan.includes(title)) return m.reply('Judul tidak tersedia!\n\nAlasan: Sudah digunakan')
+  if (catatan.includes(title)) return m.reply('Title not available!\n\nReason: Already used')
   if (!title || !isi) return m.reply(fail)
   let cttn = {
     'title': title,
     'isi': isi
   }
   global.db.data.users[m.sender].catatan.push(cttn)
-  conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/7989b4e60a9dedfcdbbec.jpg', `Catatan berhasil dibuat!\nUntuk melihat catatan. Ketik: ${usedPrefix}lihatcatatan`, wm, 'Liat Catatan', '.lihatcatatan', m)
+  conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/7989b4e60a9dedfcdbbec.jpg', `Record created successfully!\To view the record. Type: ${use Prefix}view notes`, wm, 'View notes', '.viewnotes', m)
 }
 
-handler.help = ['buatcatatan']
+handler.help = ['makeanote']
 handler.tags = ['tools']
 handler.command = /^(bu(kacatatan|at(note|ctt))|add(catatan|(note|ctt)))$/i
 
