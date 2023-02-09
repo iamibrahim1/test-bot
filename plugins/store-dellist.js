b1941897389945e44fa761698cd22b9d726d03f3
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-09 12:38:01 +0500

    Update store-dellist.js

--- plugins/store-dellist.js
+++ plugins/store-dellist.js
@@ -1,11 +1,11 @@
 let handler = async (m, { text, usedPrefix, command }) => {
-	if (!text) throw `gunakan *${usedPrefix}liststore* untuk melihat daftar pesan yg tersimpan.`
-	let msgs = global.db.data.msgs
-	if (!(text in msgs)) throw `'${text}' tidak terdaftar di daftar pesan.`
-	delete msgs[text]
-	m.reply(`\n  [💬] berhasil menghapus pesan di daftar List dengan nama '${text}'.\n`)
+	if (!text) throw `use *${usedPrefix}liststore* to see a list of saved messages.`
+let msgs = global.db.data.msgs
+if (!(text in msgs)) throw `'${text}' is not registered in the message list.`
+delete msgs[text]
+m.reply(`\n [💬] successfully deleted the message in the List with the name '${text}'.\n`)
 }
-handler.help = ['list'].map(v => 'del' + v + ' <teks>')
+handler.help = ['list'].map(v => 'del' + v + ' <text>')
 handler.tags = ['store']
 handler.command = /^dellist$/i
 handler.group = true
