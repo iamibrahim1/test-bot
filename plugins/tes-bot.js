1cf8c6d57f21adbb143d10668e74d642cfa981b3
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-02-09 12:36:33 +0500

    Update tes-bot.js

--- plugins/tes-bot.js
+++ plugins/tes-bot.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `「 𝗔𝗞𝗧𝗜𝗙 」`
+let info = `「 Status 」`
 await conn.reply(m.chat, info, m, { quoted: fakes, contextInfo: { externalAdReply: { showAdAttribution: true, title: botdate, body: bottime, sourceUrl: snh, thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
 }
 handler.customPrefix = /^(tes|tess|test)$/i
